428357cef138ae369589dc4783ed588588bf527a
DavyJ0nes <davy@forgesp.com>
2016-10-30 20:32:42 +0000

    Can now use cli args and config file

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+var fs = require('fs')
 var backup = require('./lib/backup.js')
 
-backup('/Users/davidjones', '/Volumes/Brokkr-Backup/davidjones')
+if (process.argv[2] === undefined) {
+  if (fs.statSync('./config.json').isFile()) {
+    var config = require('./config.json')
+  }
+}
+
+var src = process.argv[2] || config.src
+var dest = process.argv[3] || config.dest
+
+backup(src, dest)
